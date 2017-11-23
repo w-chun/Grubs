b22cd56d5ce29074545cb42e05be22e7a0e3712e
Wilson <wchun108@gmail.com>
2017-11-23 11:05:02 -0800

    Add demo login button

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { login } from '../../actions/session_actions';
 
 class SessionForm extends React.Component {
   constructor(props){
@@ -16,6 +17,8 @@ class SessionForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderErrors = this.renderErrors.bind(this);
     this.handleErrors = this.handleErrors.bind(this);
+    this.handleDemo = this.handleDemo.bind(this);
+    this.loginDemo = this.loginDemo.bind(this);
   }
 
 
@@ -50,27 +53,47 @@ class SessionForm extends React.Component {
     );
   }
 
+  handleDemo() {
+    return (e) => {
+      e.preventDefault();
+      this.setState({username: "demo", password: "password"}, () => {
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user, 'login');
+      });
+    };
+  }
+
+  loginDemo(){
+    return(e) => {
+      e.preventDefault();
+      const user = {username: "demo", password: "password"};
+      this.props.processForm(user, 'login');
+    };
+  }
+
   render(){
     let button;
     let message;
     let navLogin;
     let navSignup;
     let emailForm;
+    let demoLogin;
     if (this.props.formType === "signup") {
       button = 'Sign Up';
       message = 'Sign Up for Grubs';
       emailForm = (
-        <label>Email:
           <input type="text"
           value={this.state.email}
-          onChange={this.update("email")} />
-        </label>
+          onChange={this.update("email")}
+          placeholder="Email" />
       );
       navLogin = (<p>Already on Grubs? <Link className="nav-login-link" to='/login'>Log In</Link></p>);
+      demoLogin = <button className="demo-login" onClick={this.loginDemo()}>Demo Login</button>;
     } else {
       button = 'Log In';
       message = 'Log In to Grubs';
       navSignup = (<p>New to Grubs? <Link className="nav-login-signup" to='signup'>Sign Up</Link></p>);
+      demoLogin = <button className="demo-login" onClick={this.handleDemo()}>Demo Login</button>;
     }
     return (
       <div className="session-form-container">
@@ -86,25 +109,24 @@ class SessionForm extends React.Component {
                 <h2 className="message">{message}</h2>
                 <div className="nav-login" onClick={this.handleErrors}>{navLogin}</div>
                 <form onSubmit={this.handleSubmit} className="login-form-box">
-                    <label>Username:
-                      <input type="text"
-                        value={this.state.username}
-                        onChange={this.update("username")} />
-                    </label>
+                    <input type="text"
+                      value={this.state.username}
+                      onChange={this.update("username")}
+                      placeholder="Username" />
                     {emailForm}
-                    <label>Password:
-                      <input type="password"
-                        value={this.state.password}
-                        onChange={this.update("password")} />
-                    </label>
+                    <input type="password"
+                      value={this.state.password}
+                      onChange={this.update("password")}
+                      placeholder="Password" />
 
                     <input type="submit" value={button} className="button"/>
+                    {demoLogin}
                 </form>
                 <div className="nav-signup" onClick={this.handleErrors}>{navSignup}</div>
               </div>
             </div>
             <div className="session-img">
-              <img src="https://s3-media4.fl.yelpcdn.com/assets/2/www/img/1e82406ff345/signup/signup_illustration.png"></img>
+              <img src="http://res.cloudinary.com/dih798zsl/image/upload/v1511460944/signup_illustration_gjosky.png"></img>
             </div>
         </div>
       </div>
