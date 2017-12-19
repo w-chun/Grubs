e846d8dbd557cb87b6ba5d786506958c94009eff
Wilson <wchun108@gmail.com>
2017-12-18 16:04:15 -0800

    Add additional business info

--- frontend/components/business_show/business_show.jsx
+++ frontend/components/business_show/business_show.jsx
@@ -74,6 +74,47 @@ export default class BusinessShow extends React.Component {
           </div>
           <div className="bottom-container">
             <ReviewIndexContainer />
+            <div className="bottom-container-2-3">
+              <h3 className="business-info-text">Hours</h3>
+              <ul className="business-info-list">
+                <li><b>Mon</b>     10:30 am - 10:00 pm</li>
+                <li><b>Tues</b>    10:30 am - 10:00 pm</li>
+                <li><b>Wed</b>     10:30 am - 10:00 pm</li>
+                <li><b>Thur</b>    10:30 am - 10:00 pm</li>
+                <li><b>Fri</b>     10:30 am - 10:00 pm</li>
+                <li><b>Sat</b>     10:30 am - 10:00 pm</li>
+                <li><b>Sun</b>     10:30 am - 10:00 pm</li>
+
+              </ul>
+              <h3 className="business-info-text">More business info</h3>
+              <ul className="business-info-list">
+                <li>Takes Reservations  <b>Yes</b></li>
+                <li>Delivery  <b>No</b></li>
+                <li>Take-out  <b>Yes</b></li>
+                <li>Accepts Credit Cards  <b>Yes</b></li>
+                <li>Accepts Android Pay <b>No</b></li>
+                <li>Accepts Apple Pay  <b>No</b></li>
+                <li>Accepts Bitcoin  <b>No</b></li>
+                <li>Good For  <b>Lunch, Dinner</b></li>
+                <li>Parking  <b>Street</b></li>
+                <li>Bike Parking  <b>No</b></li>
+                <li>Wheelchair Accessible  <b>Yes</b></li>
+                <li>Good for Kids  <b>Yes</b></li>
+                <li>Good for Groups  <b>Yes</b></li>
+                <li>Attire  <b>Casual</b></li>
+                <li>Ambience  <b>Trendy</b></li>
+                <li>Noise Level  <b>Average</b></li>
+                <li>Alcohol  <b>Full Bar</b></li>
+                <li>Outdoor Seating  <b>No</b></li>
+                <li>Wi-Fi  <b>No</b></li>
+                <li>Has TV  <b>No</b></li>
+                <li>Dogs Allowed  <b>No</b></li>
+                <li>Waiter Service  <b>Yes</b></li>
+                <li>Drive-Thru  <b>No</b></li>
+                <li>Caters  <b>No</b></li>
+                <li>Gender Neutral Restrooms  <b>Yes</b></li>
+              </ul>
+            </div>
           </div>
           </div>
         </div>
@@ -83,3 +124,16 @@ export default class BusinessShow extends React.Component {
     }
   }
 }
+
+// <table>
+//   <tbody>
+//     <tr><th>Mon</th><td>10:30 am - 10:00 pm</td></tr>
+//     <tr><th>Tue</th><td>10:30 am - 10:00 pm</td></tr>
+//     <tr><th>Wed</th><td>10:30 am - 10:00 pm</td></tr>
+//     <tr><th>Thu</th><td>10:30 am - 10:00 pm</td></tr>
+//     <tr><th>Fri</th><td>10:30 am - 10:00 pm</td></tr>
+//     <tr><th>Sat</th><td>10:30 am - 10:00 pm</td></tr>
+//     <tr><th>Sun</th><td>10:30 am - 10:00 pm</td></tr>
+//   </tbody>
+// </table>
+//
