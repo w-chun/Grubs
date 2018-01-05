485b2911b4849d39fc62461f7e91aeba993f68c7
Wilson <wchun108@gmail.com>
2018-01-05 13:29:37 -0800

    Remove footer links and fix styling

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -8,57 +8,44 @@ export default class Footer extends React.Component{
         <div className="footer-container">
           <div className="footer-menu">
             <ul>
-              <h3>About</h3>
-              <li><a href="https://w-chun.github.io/portfolio/" target="_blank">About Grubs</a></li>
-              <li><a>Careers</a></li>
-              <li><a>Press</a></li>
-              <li><a>Investor Relations</a></li>
-              <li><a>Content Guidelines</a></li>
-              <li><a>Terms of Service</a></li>
-              <li><a>Privacy Policy</a></li>
-              <li><a>Ad Choices</a></li>
+              <h3>About Me</h3>
+              <li><a href="https://w-chun.github.io/portfolio/" target="_blank"><div className="website"></div></a></li>
             </ul>
           </div>
           <div className="footer-menu">
             <ul>
-              <h3>Discover</h3>
-              <li><a href="https://w-chun.github.io/portfolio/" target="_blank">The Local Grubs</a></li>
-              <li><a>Grubs Blog</a></li>
-              <li><a>Support</a></li>
-              <li><a>Grubs Mobile</a></li>
-              <li><a>Developer</a></li>
-              <li><a>RSS</a></li>
+              <h3>LinkedIn</h3>
+              <a href="https://www.linkedin.com/in/wilson-chun/" target="_blank"><div className="linkedin"></div></a>
             </ul>
           </div>
           <div className="footer-menu">
             <ul>
-              <h3>Grubs for Business Owners</h3>
-              <li><a href="https://w-chun.github.io/portfolio/" target="_blank">Claim your Business Page</a></li>
-              <li><a>Advertise on Grubs</a></li>
-              <li><a>Grubs Reservations</a></li>
-              <li><a>Business Success Stories</a></li>
-              <li><a>Business Support</a></li>
-              <li><a>Grubs Blog For Business Owners</a></li>
+              <h3>Github</h3>
+              <a href="https://github.com/w-chun" target="_blank"><div className="github"></div></a>
             </ul>
           </div>
-          <div className="linkedin-github">
-            <div className="footer-menu">
-              <ul>
-                <h3>LinkedIn</h3>
-                <a href="https://www.linkedin.com/in/wilson-chun/" target="_blank"><div className="linkedin"></div></a>
-              </ul>
-            </div>
-            <div className="footer-menu">
-              <ul>
-                <h3>Github</h3>
-                <li><a href="https://github.com/w-chun" target="_blank">
-                  <div className="github"></div>
-                    </a></li>
-              </ul>
-            </div>
-          </div>
+        </div>
+        <div className="footer-img-container">
+          <div className="footer-image"></div>
         </div>
       </div>
     );
   }
 }
+
+// <div className="linkedin-github">
+//   <div className="footer-menu">
+//     <ul>
+//       <h3>LinkedIn</h3>
+//       <a href="https://www.linkedin.com/in/wilson-chun/" target="_blank"><div className="linkedin"></div></a>
+//     </ul>
+//   </div>
+//   <div className="footer-menu">
+//     <ul>
+//       <h3>Github</h3>
+//       <li><a href="https://github.com/w-chun" target="_blank">
+//         <div className="github"></div>
+//           </a></li>
+//     </ul>
+//   </div>
+// </div>
